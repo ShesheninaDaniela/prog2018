5f217415b65318ea3e291809493389c51f824b29
ShesheninaDaniela <fdsa93@outlook.com>
2018-04-03 19:37:14 +0500

    Task3

---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program
+{
+    public class HashTable
+    {
+        public List<int> hashList;
+        public List<List<Information>> hashTable;
+
+        public class Information
+
+        {
+            public object Key { get; set; }
+            public object Value { get; set; }
+        }
+         /// Поиск индекса хэш-кода
+       
+        public int SearchHashIndex(int hashCode)
+        {
+            return hashList.IndexOf(hashCode);
+        }
+        /// Создание новой хэш-таблицы
+        
+        public void CreateNewHashTable(int size)
+        {
+            hashTable = new List<List<Information>>(size);
+            hashList = new List<int>(size);
+            for (int i = 0; i < size; i++)
+            {
+                hashTable.Add(new List<Information>());
+            }
+        }
+
+       
+        /// Получение хэш-кода ключа
+        
+        public int GetHashCode(object key)
+        {
+            return key.GetHashCode();
+        }
+     
+        /// Добавление новой пары в хэш-таблицу
+      
+        public void PutPair(object key, object value)
+        {
+            var hashCode = GetHashCode(key);
+            var hashIndex = SearchHashIndex(hashCode);
+            var keyValue = new Information
+              { Key = key, Value = value };
+            if (hashIndex == -1 && hashTable.Count > hashList.Count)
+            {
+                hashList.Add(hashCode);
+                hashIndex = SearchHashIndex(hashCode);
+                hashTable[hashIndex].Add(keyValue);
+                return;
+            }
+            foreach (var pair in hashTable[hashIndex])
+            {
+                if (pair.Key.Equals(key))
+                    pair.Value = value;
+            }
+        }
+
+         /// Получение значения по ключу
+        
+        public object GetValueByKey(object key)
+        {
+            var index = SearchHashIndex(GetHashCode(key));
+            if (index == -1)
+                return null;
+            foreach (var pair in hashTable[index])
+            {
+                if (pair.Key.Equals(key))
+                    return pair.Value;
+            }
+            return null;
+        }
+
+        public static void Main()
+        {
+           
+        }
+    }
+}
+
+
---
+++ HashTable/HashTableTest/UnitTest1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Program;
+using System.Collections.Generic;
+
+namespace TestHashTable
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        //Добавление и поиск трёх элементов
+        public void TestThreeElements()
+        {
+            var hashTest = new HashTable();
+            var list = new List<string>();
+            hashTest.CreateNewHashTable(3);
+            hashTest.PutPair("первый элемент", "проверка");
+            hashTest.PutPair(0, 1234);
+            hashTest.PutPair("first", list);
+
+            var keys = new object[] { "первый элемент", 0, "first" };
+            var values = new object[] { "проверка", 1234, list };
+            for (int i = 0; i < 3; i++)
+            {
+                var a = hashTest.GetValueByKey(keys[i]);
+                var b = values[i];
+                if (!(a).Equals(b)) throw new Exception();
+            }
+        }
+
+        [TestMethod]
+        //Добавление одного и того же ключа дважды с разными значениями сохраняет последнее добавленное значение
+        public void TestTwoElementsWithSameKey()
+        {
+            var hashTest = new HashTable();
+            hashTest.CreateNewHashTable(2);
+            hashTest.PutPair("первый элемент", "проверка");
+            hashTest.PutPair("первый элемент", "erroneous");
+            var key = "первый элемент";
+            var value = "erroneous";
+            var a = hashTest.GetValueByKey(key);
+            if (!(a).Equals(value)) throw new Exception();
+        }
+
+        [TestMethod]
+        //Поиск одного элемента из тысячи
+        public void TestOneElementAmongTenThousand()
+        {
+            var size = 10000;
+            var table = new HashTable();
+            table.CreateNewHashTable(size);
+            for (var i = 0; i < size - 1; i++)
+            {
+                Random rnd = new Random(DateTime.Now.Millisecond);
+                Random rnd1 = new Random(DateTime.Now.Millisecond);
+                var key = Math.Pow(i, 2);
+                var element = rnd.Next(567, 123423) - rnd1.Next(56, 1234);
+                table.PutPair(key, element);
+            }
+            table.PutPair(0, 12345);
+            var a = table.GetValueByKey(0);
+            var b = 12345;
+            if (a != null)
+            {
+                if (!(a.Equals(b))) throw new Exception();
+            }
+        }
+
+        [TestMethod]
+        //Поиск тысячи недобавленныех ключей
+        public void TestSearchingNotAddedElements()
+        {
+            var size = 10000;
+            var table = new HashTable();
+            table.CreateNewHashTable(size);
+            for (var i = 0; i < size; i++)
+            {
+                Random rnd = new Random(DateTime.Now.Millisecond);
+                Random rnd1 = new Random(DateTime.Now.Millisecond);
+                var key = Math.Pow(i, 2);
+                var element = rnd.Next(567, 123423) - rnd1.Next(56, 1234);
+                table.PutPair(key, element);
+            }
+            for (int i = size; i < size + 1000; i++)
+            {
+                if (!(table.GetValueByKey(i) == null))
+                {
+                    throw new Exception();
+                }
+            }
+        }
+    }
+}
+
